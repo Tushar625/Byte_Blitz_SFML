bdd4a725cf13d8d2cb4ad2da2ee06c771f12f9dc
Tushar Das <td9940433@gmail.com>
2024-10-28 21:24:41 +0530

    upgraded documentation for state machine

--- state_machine/state_machine.h
+++ state_machine/state_machine.h
@@ -1,42 +1,132 @@
 #pragma once
 
+#include<limits>
+
 
 /*
 	how to use:
 
-	each game state is a class inherited ("public inheritance") from BASE_STATE class, with redefinition
-	of necessary functions.
+	create an object of STATE_MACHINE class. Its constructor takes nothing.
 
-	create objects of those state classes (global or local).
+	Example: STATE_MACHINE sm;
 
-	now create an object of STATE_MACHINE class (global or local). Its constructor
-	takes a list of pointers game states as argument. These are pointers to those
-	state objects created in previous step.
+	now create the states, each state is a class inherited ("public inheritance") from BASE_STATE
+	class, with proper definition of Enter(), Update(), Render(), Exit() functions, and create objects
+	of those 'state classes'.
 
-	Example: STATE_MACHINE sm({&state0, &state1, &state2, &state3});
+	Example:
 
-	[note, type of each state is different but they are inherited from the same BASE_STATE class so, a BASE_STATE
-	type pointer can hold their address]
+	class initial_state : public BASE_STATE
+	{
+		void Enter()	// initialze this state
+		{...}
+
+		int Update(double dt)	// update this state
+		{...}
+
+		void Render()	// render this state
+		{...}
+
+		void Exit()		// destroy this state
+		{...}
+	} initial;
+
+	inside the Update() you call "sm.change_to()" to change the current state of the state machine
+	this function takes a reference to a state object and makes it the current state. this function
+	is also used to set the initial state of the state machine.
+
+	note: a call to "sm.change_to()" from Update() of a state must be last action of this function
+	as, sm.change_to() calls the Exit() of current state i.e., destroys the current state
+
+	Example:
+
+	extern class play_state play;	// declaring the state object defined later
+
+	// ~~~~~ initial state ~~~~~
+
+	class initial_state : public BASE_STATE
+	{
+		...
+
+		int Update(double dt)
+		{
+			...
+			sm.change_to(play);	// change state: 'initial' -> 'play' 
+			...
+		}
+
+		...
+	};
+	
+	initial_state initial;
+
+	// ~~~~~ play state ~~~~~ 
+	
+	class play_state : public BASE_STATE
+	{
+		...
+	};
+	
+	play_state play;
+	
+	// the function that initializes the game
+
+	Game::Create()
+	{
+		...
+		sm.change_to(initial);	// setting 'intial' as initial state
+		...
+	}
 
 	call the update and render methods of STATE_MACHINE from update and render
-	functions of the game loop.
+	functions of the game loop, that inturn call the update and render methods
+	of current state.
 
-	by default the state machine constructor doesn't set any state as the initial state so use
-	change_to() to set the initial state, after a state machine object is created.
+	Update() of the states can also return 'int' data, which is again returned
+	by the Update() of state machine to Update() of game loop, you can use this
+	signal the game loop to stop; if no current state is set, Update() of state
+	machine returns the minimum int value, a garbage value.
 
-	and its done.
+	Example:
 
-	adv: entire description of a state gets enclosed in the state classes
+	class initial_state : public BASE_STATE
+	{
+		...
+
+		int Update(double dt)
+		{
+			...
+			if(...)
+			{
+				return 0;	// using '0' to signal exit
+			}
+			...
+			return -1;
+		}
+
+		...
+	};
+
+	...
+
+	bool Game::Update(double dt)
+	{
+		...
+		if(sm.Update(dt) == 0)
+		{
+			// code to stop the game loop
+			...
+		}
+		...
+	}
 */
 
 
 /*
-	this ia basically a template for game states,
+	this is a template for game states,
 
-	basically game states are classes that inherites this
-	class and redefine the virtual methods as per their requirement
-
-	watch how we use Update method to change the game state
+	game states are actually classes that inherite this class and redefine the
+	virtual methods as per their requirements
 */
 
 class BASE_STATE
@@ -46,23 +136,10 @@ class BASE_STATE
 	virtual void Enter()
 	{}
 
-	/*
-		returns the index of the next state if state change is needed
-		else return -1
-
-		i.e., according to the index of the state returned by it Update
-		method of STATE_MACHINE changes the game state
-	*/
-
 	virtual int Update(double dt)
 	{
-		return -1;
+		return std::numeric_limits<int>::min();
 	}
-
-	/*
-		we don't sent the fps string to the rander as it would increase
-		the typing effort as there will be many game states
-	*/
 
 	virtual void Render()
 	{}
@@ -73,8 +150,8 @@ class BASE_STATE
 
 
 /*
-	this is the actual state machine, it mentains an array
-	that points to all the game states
+	this is the actual state machine, it has a BASE_STATE pointer that points the
+	current state
 */
 
 class STATE_MACHINE
@@ -94,7 +171,7 @@ class STATE_MACHINE
 
 	/*
 		state transition
-		exit from current state and set the current sate as ith state
+		exit from current state and set the next sate as its current state
 	*/
 
 	template<typename STATE_TYPE>
@@ -112,8 +189,9 @@ class STATE_MACHINE
 
 	/*
 		gets elapsed time from game loop and supplies it to current state
-		returns the new state set by update function of current state
-		-1 if no current state found
+		returns an int to the Update() of game loop, update() of the game
+		states can use this to send some signal (eg, stop game loop) to
+		the Update() of game loop
 	*/
 
 	int Update(double dt)
@@ -123,12 +201,12 @@ class STATE_MACHINE
 			return current_state->Update(dt);
 		}
 
-		return -1;
+		return std::numeric_limits<int>::min();
 	}
 
 
 	/*
-		rander the state on a frame f1
+		rander the state
 	*/
 
 	void Render()
