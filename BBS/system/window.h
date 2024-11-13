fae5d5524e15bfd7c7a9be211d8db86224080a32
Tushar Das <td9940433@gmail.com>
2024-11-13 14:38:16 +0530

    minor optimisations

--- BBS/system/window.h
+++ BBS/system/window.h
@@ -6,13 +6,22 @@
 namespace bb
 {
 
-	sf::ContextSettings setting;
-
 	unsigned int set_antialiashing();
 
 	// this object, WINDOW, should be used to access or modify the SFML window
 
-	sf::RenderWindow WINDOW(sf::VideoMode(800, 600), "Default Window 800ps-600px, Enjoy :)", sf::Style::Close, (setting.antialiasingLevel = set_antialiashing(), setting));
+	sf::RenderWindow WINDOW(
+		sf::VideoMode(800, 600),
+		"Default Window 800ps-600px, Enjoy :)",
+		sf::Style::Close,
+		([]() -> sf::ContextSettings
+			{
+				sf::ContextSettings setting;
+				setting.antialiasingLevel = set_antialiashing();
+				return setting;
+			}
+		)()	/*creating this lambda expression to return sf::ContextSettings object and calling it*/
+	);
 
 }
 
